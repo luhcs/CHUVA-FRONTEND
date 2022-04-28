ddbe99c685e199dc31483a39b6bee8c00c51a22e
luhcs <lucas_fgomes@CAMP-CMP-01.localdomain>
2022-04-28 18:09:19 -0300

    Altera index.html, script.js, styles.css e video.mp4. Adiciona arquivos as pastas "img" e "videos".
    ├── assets
    |  ├── img
    |  |  ├── dewey1.png *
    |  |  ├── dewey4.png
    |  |  ├── doi.svg
    |  |  ├──icone1.png *
    |  |  ├──icone2.png *
    |  |  ├──icone3.png *
    |  |  └── profile.png
    |  ├── videos
    |  |  ├── video.mp4
    |  |  └── video.vtt *
    
    index.html :
    
    	- Adiciona a seção do Video
    	- Adiciona a seção do Resumo e Discussão
    	- Importa o script Plyr 3.7.2 Polyfilled
    	- Importa o script jQuery 3.6.0 minified
    
    scripts.js :
    
    	- Cria a constante "player" que renderiza um novo player
    	utilizando o framework "Plyr";
    	- Cria o event handler para exibir ou comprimir o conteudo do
    	componente "Resumo" quando clicado, utilizando jQuery.
    
    styles.css:
    
    	- Adiciona estilos aos componentes Video, Resumo e Discussão.
    
    video.mp4 :
    
    	- Altera o conteúdo.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
 const dropdownMenu = () => {
-    
+
 }
+
+const player = new Plyr('#player', {
+    title: 'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP',
+    controls: [
+        'play-large',
+        'play',
+        'progress',
+        'current-time',
+        'mute',
+        'volume',
+        'captions',
+        'settings',
+        'fullscreen'
+    ],
+});
+
+window.player = player;
+
+$("#sum").on("click", function () {
+
+
+    if ($("#expand").css("display") === "none") {
+        $("#expand").css("display", "inline");
+        $("#ellipsis").css("display", "none");
+    } else {
+        $("#ellipsis").css("display", "inline");
+        $("#expand").css("display", "none");
+    }
+});
